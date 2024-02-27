4b586d08563cc6ead6ec31b314b7dbbc9502d753
rjmn02 <108502756+rjmn02@users.noreply.github.com>
2024-02-27 20:49:14 +0800

    ss

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+
+//https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+//ambot potangina
