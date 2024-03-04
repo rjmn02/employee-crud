b466a559e0acdb929a9d06e91b74bd16c02a49e2
rjmn02 <108502756+rjmn02@users.noreply.github.com>
2024-03-05 02:37:50 +0800

    var cgabge

--- app/api/employees.ts
+++ app/api/employees.ts
@@ -68,7 +68,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             id: parseInt(req.query.id as string)
           }
         });
-        res.status(200).json(`employee {message: 'Employee Deleted'}`);
+        res.status(200).json(employee);
         break;
       }
       res.status(400).json({message: 'No ID'});
